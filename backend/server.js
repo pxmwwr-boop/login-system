6c2c365096a095a7c0092655c5cb7e165c41be5c
sszu <pxmwwr@gmail.com>
2026-09-24 21:24:57 +0800

    Lock down CORS

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 
 const userSchema = new mongoose.Schema({
